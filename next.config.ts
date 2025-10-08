739a46d2f716c67d19b5f991aaa9178c2d45270a
Aseel Alwjeeh <aseelalwjeeh6@gmail.com>
2025-10-08 15:00:51 +0000

    "استبدال وتحسين أداء الخلفيات: يجب إلغاء جميع الخلفيات المقترحة أو المست

--- next.config.ts
+++ next.config.ts
@@ -52,6 +52,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www2.0zz0.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
